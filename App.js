a2f6bc89224a2f8eea294d246ef32ab0fbebb0ab
lucky <luckyrathore03@gmail.com>
2023-02-24 14:24:54 +0530

    paper + picker build succeed, note: otp screen installed

--- App.js
+++ App.js
@@ -1,29 +1,127 @@
-// In App.js in a new project
-
 import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import ImagePickerExample from './ImagePickerExample';
+import { useState, useRef } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { TouchableOpacity } from 'react-native';
+import {
+  Card,
+  Title,
+  Paragraph,
+  List,
+  Provider as PaperProvider,
+  Button
+} from 'react-native-paper';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+import { Picker } from '@react-native-picker/picker';
+
+
+
+import { Text, Image, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+
+import OTPTextView from 'react-native-otp-textinput';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Stack = createNativeStackNavigator();
 
-function App() {
+const title = 'Hello';
+const content = 'world';
+
+const HomeScreen = ({ navigation }) => (
+  <TouchableOpacity
+    onPress={() =>
+      navigation?.push('Details', {
+        title,
+        content,
+      })
+    }
+  >
+    <Card>
+      <Card.Content>
+        <Title>{title}</Title>
+        <Paragraph>{content}</Paragraph>
+      </Card.Content>
+    </Card>
+  </TouchableOpacity>
+);
+
+const DetailsScreen = (props) => {
+  const { title, content } = props?.route?.params;
+  const [selectedLanguage, setSelectedLanguage] = useState();
+  const [image, setImage] = useState(null);
+
+
+  AsyncStorage.setItem(1,2);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  const pickerRef = useRef();
+
+  function open() {
+    pickerRef.current.focus();
+  }
+
+  function close() {
+    pickerRef.current.blur();
+  }
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="ImagePickerExample" component={ImagePickerExample} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <List.Section>
+      <List.Subheader>{title}</List.Subheader>
+      <Text>13</Text>
+      <View>
+        <Button mode='contained' title="Pick an image from camera roll" onPress={pickImage} />
+        {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      </View>
+      <Text>13</Text>
+      <Picker
+        ref={pickerRef}
+        selectedValue={selectedLanguage}
+        onValueChange={(itemValue, itemIndex) =>
+          setSelectedLanguage(itemValue)
+        }>
+        <Picker.Item label="Java" value="java" />
+        <Picker.Item label="JavaScript" value="js" />
+      </Picker>
+
+      <OTPTextView
+        handleTextChange={i => console.log(i)}
+        containerStyle={styles.textInputContainer}
+        textInputStyle={styles.roundedTextInput}
+        inputCount={6}
+        inputCellLength={1}
+        tintColor="#000"
+      />
+
+    </List.Section>
+  );
+};
+
+export default function App() {
+  return (
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
 
-export default App;
